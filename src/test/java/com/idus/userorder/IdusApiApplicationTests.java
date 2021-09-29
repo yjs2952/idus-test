e4265f6eea795693918e2699c1fe9689d2c12f97
yjs2952 <yjs2952@naver.com>
2021-09-29 21:14:44 +0900

    Initial commit

---
+++ src/test/java/com/idus/userorder/IdusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idus.userorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
